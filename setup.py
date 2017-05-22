e52dd49e24a00e3234eaa18b2c0587dc3479593e
Andrew Berger <andberger@berkeley.edu>
2017-05-22 17:10:13 -0400

    added minimal setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+""" Setup script
+"""
+
+from distutils.core import setup
+
+setup(name='ipp-tools',
+      version='0.01',
+      description='IPyParallel utilities',
+      author='Andrew Berger',
+      author_email='bergera@janelia.hhmi.org',
+      url='https://github.com/TuragaLab/ipp-tools',
+      py_modules=['ipp_tools'])
