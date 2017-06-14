c3306d9340f404517a8a7ec5a3aee85371ed316d
Andrew Berger <andberger@berkeley.edu>
2017-06-14 19:52:25 -0400

    wip map interface to slurm cluster

---
+++ templates/slurm_template.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --job-name={job_name}
+#SBATCH --output={output_path}
+#
+#SBATCH --ntasks={n_tasks}
+#SBATCH --time=0
+#SBATCH --mem-per-cpu={mem_mb}
+#SBATCH --cpus-per-task={n_cpus}
+#SBATCH --gres=gpu:{n_gpus}
+
+srun source activate {dev_env}; ipengine --profile={profile} --location={controller_hostname} &
