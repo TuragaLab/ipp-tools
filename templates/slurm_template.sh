88fc3e1e78c4a82bcf0d323088513f6664564c65
Andrew Berger <andberger@berkeley.edu>
2017-06-15 17:18:02 -0400

    fix sbatch syntax

--- templates/slurm_template.sh
+++ templates/slurm_template.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem-per-cpu={mem_mb}
 #SBATCH --cpus-per-task={n_cpus}
 #SBATCH --gres=gpu:{n_gpus}
-#SBATCH --comment:"{comment}"
+#SBATCH --comment="{comment}"
 
 source activate {dev_env}
 srun ipengine --profile={profile} --location={controller_hostname} --cluster-id={cluster_id}&
